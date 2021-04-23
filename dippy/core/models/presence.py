3a1a32965b0e87a43abe480ef8f7b7496d2e9854
Zech Zimmerman <hi@zech.codes>
2021-04-23 16:43:31 -0400

    Use validator to get user ID from user partial

--- dippy/core/models/presence.py
+++ dippy/core/models/presence.py
@@ -4,7 +4,7 @@ from dippy.core.models.client_status import ClientStatusModel
 from dippy.core.models.game import GameModel
 from dippy.core.models.model import DippyCoreModel
 from dippy.core.snowflake import Snowflake
-from pydantic import Field
+from pydantic import Field, validator
 from typing import Optional
 
 
@@ -14,3 +14,7 @@ class PresenceModel(DippyCoreModel):
     game: Optional[GameModel]
     client_status: ClientStatusModel
     activities: list[ActivityModel]
+
+    @validator("user_id", pre=True)
+    def get_user_id(cls, value):
+        return Snowflake(value.get("id"))
