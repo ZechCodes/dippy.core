a0feb3363b96bf9dfdbb632ad3b3ec4c0090c7f1
Zech Zimmerman <hi@zech.codes>
2021-04-24 09:51:49 -0400

    Add op codes as an enum

--- dippy/core/enums.py
+++ dippy/core/enums.py
@@ -5,6 +5,31 @@ class StrEnum(str, Enum):
     ...
 
 
+class GatewayCode(IntEnum):
+    # An event was dispatched.
+    DISPATCH = 0
+    # Fired periodically by the client to keep the connection alive.
+    HEARTBEAT = 1
+    # Starts a new session during the initial handshake.
+    IDENTIFY = 2
+    # Update the client's presence.
+    PRESENCE_UPDATE = 3
+    # Used to join/leave or move between voice channels.
+    VOICE_STATE_UPDATE = 4
+    # Resume a previous session that was disconnected.
+    RESUME = 6
+    # You should attempt to reconnect and resume immediately.
+    RECONNECT = 7
+    # Request information about offline guild members in a large guild.
+    REQUEST_GUILD_MEMBERS = 8
+    # The session has been invalidated. You should reconnect and identify/resume accordingly.
+    INVALID_SESSION = 9
+    # Sent immediately after connecting, contains the heartbeat_interval to use.
+    HELLO = 10
+    # Sent in response to receiving a heartbeat to acknowledge that it has been received.
+    HEARTBEAT_ACK = 11
+
+
 class ChannelType(IntEnum):
     TEXT = 0
     DM = 1
