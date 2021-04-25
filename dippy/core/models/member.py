4ed8e1dfd58309ebb0c8c812474dc52ba7cb21b1
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:20:17 -0400

    Clean up and add missing field to the member model

--- dippy/core/models/member.py
+++ dippy/core/models/member.py
@@ -1,18 +1,27 @@
 from datetime import datetime
-from dippy.core.models.model import DippyCoreModel
+from dippy.core.models.model import DippyCoreCacheableModel
 from dippy.core.models.user import UserModel
 from dippy.core.snowflake import Snowflake
+from pydantic import Field
 from typing import Optional
 
 
-class MemberModel(DippyCoreModel):
-    guild_id: Optional[Snowflake]
-    user: Optional[UserModel]
-    nick: Optional[str]
-    roles: list[Snowflake]
-    joined_at: datetime
-    premium_since: Optional[datetime]
+class MemberModel(DippyCoreCacheableModel):
     deaf: bool
+    joined_at: datetime
     mute: bool
-    is_pending: Optional[bool]
+    roles: list[Snowflake]
+
+    guild_id: Optional[Snowflake]
     hoisted_role: Optional[Snowflake]
+    nick: Optional[str]
+    pending: Optional[bool] = Field(alias="is_pending")
+    permissions: Optional[str]
+    premium_since: Optional[datetime]
+    user: Optional[UserModel]
+
+    @property
+    def id(self) -> Optional[int]:
+        if self.user:
+            return self.user.id
+        return
