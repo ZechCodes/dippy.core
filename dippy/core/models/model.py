49a5eb9d634a7ff4e0cc867811c541c918e58228
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:19:25 -0400

    Add a base model for cacheable models

--- dippy/core/models/model.py
+++ dippy/core/models/model.py
@@ -1,5 +1,11 @@
-from pydantic import BaseModel
+from dippy.core.caching.cacheable import Cacheable
+from dippy.core.timestamp import Timestamp
+from pydantic import BaseModel, Field
 
 
 class DippyCoreModel(BaseModel):
     pass
+
+
+class DippyCoreCacheableModel(DippyCoreModel, Cacheable):
+    created: Timestamp = Field(default_factory=Timestamp)
