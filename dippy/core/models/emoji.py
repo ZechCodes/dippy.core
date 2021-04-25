4e5f92daeb872f9bb49fa0eb293b3c465803e64d
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:20:40 -0400

    Cleanup the emoji model

--- dippy/core/models/emoji.py
+++ dippy/core/models/emoji.py
@@ -1,16 +1,17 @@
-from dippy.core.models.model import DippyCoreModel
+from dippy.core.models.model import DippyCoreCacheableModel
 from dippy.core.models.role import RoleModel
 from dippy.core.models.user import UserModel
 from dippy.core.snowflake import Snowflake
 from typing import Optional
 
 
-class EmojiModel(DippyCoreModel):
-    id: Optional[Snowflake]
+class EmojiModel(DippyCoreCacheableModel):
     name: str
-    roles: Optional[list[RoleModel]]
-    user: Optional[UserModel]
-    require_colons: Optional[bool]
-    managed: Optional[bool]
+
     animated: Optional[bool]
     available: Optional[bool]
+    id: Optional[Snowflake]
+    managed: Optional[bool]
+    require_colons: Optional[bool]
+    roles: Optional[list[RoleModel]]
+    user: Optional[UserModel]
