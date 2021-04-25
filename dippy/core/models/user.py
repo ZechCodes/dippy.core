5820febe212857bac54e47b5027c0db82b9b5f2a
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:18:34 -0400

    Reorganize the user model and add missing fields

--- dippy/core/models/user.py
+++ dippy/core/models/user.py
@@ -1,11 +1,18 @@
-from dippy.core.models.model import DippyCoreModel
+from dippy.core.enums import UserFlag
+from dippy.core.models.model import DippyCoreCacheableModel
 from dippy.core.snowflake import Snowflake
 from typing import Optional
 
 
-class UserModel(DippyCoreModel):
-    avatar: Optional[str]
-    bot: Optional[bool]
-    discriminator: str
+class UserModel(DippyCoreCacheableModel):
     id: Snowflake
     username: str
+    discriminator: str
+    avatar: Optional[str]
+    bot: Optional[bool]
+    system: Optional[bool]
+    mfa_enabled: Optional[bool]
+    locale: Optional[str]
+    verified: Optional[bool]
+    email: Optional[str]
+    flags: Optional[UserFlag]
