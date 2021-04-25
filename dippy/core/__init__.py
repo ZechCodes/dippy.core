2209281d40bd219ac53f10682fba409df0d44666
Zech Zimmerman <hi@zech.codes>
2021-04-25 11:11:15 -0400

    Add event dispatch to package imports

--- dippy/core/__init__.py
+++ dippy/core/__init__.py
@@ -1,7 +1,8 @@
+from dippy.core.caching.manager import CacheManager
+from dippy.core.events import EventDispatch
 from dippy.core.gateway.connection import GatewayConnection
 from dippy.core.gateway.payload import Payload
 from dippy.core.intents import Intents
-from dippy.core.caching.manager import CacheManager
 
 
-__all__ = ["CacheManager", "GatewayConnection", "Intents"]
+__all__ = ["CacheManager", "EventDispatch", "GatewayConnection", "Intents"]
