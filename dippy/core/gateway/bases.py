bffeb5d4be8781b45da6ea72a0af6523bbe20686
Zech Zimmerman <hi@zech.codes>
2021-04-25 11:07:46 -0400

    Add abstract bases for the gateway and heartbeat

---
+++ dippy/core/gateway/bases.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from bevy import Injectable
+from dippy.core.events import BaseEventStream
+from dippy.core.gateway.payload import Payload
+from typing import Optional
+
+
+class BaseGatewayConnection(ABC, Injectable):
+    @property
+    @abstractmethod
+    def connected(self) -> bool:
+        ...
+
+    @property
+    @abstractmethod
+    def gateway(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def sequence_index(self) -> Optional[int]:
+        ...
+
+    @abstractmethod
+    async def connect(self):
+        ...
+
+    @abstractmethod
+    async def disconnect(self, code: int):
+        ...
+
+    @abstractmethod
+    async def resume(self):
+        ...
+
+    @abstractmethod
+    async def send(self, payload: Payload):
+        ...
+
+
+class BaseHeartbeat(ABC, Injectable):
+    @property
+    @abstractmethod
+    def running(self) -> bool:
+        ...
+
+    @abstractmethod
+    def stop(self):
+        ...
