0f22f5f061b8e72d4a864c72829dffcc8b2f31fc
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:25:23 -0400

    Add user flag enum

--- dippy/core/enums.py
+++ dippy/core/enums.py
@@ -238,3 +238,19 @@ class PremiumTier(IntEnum):
     TIER_1 = 1  # Guild has unlocked Server Boost level 1 perks
     TIER_2 = 2  # Guild has unlocked Server Boost level 2 perks
     TIER_3 = 3  # Guild has unlocked Server Boost level 3 perks
+
+
+class UserFlag(IntEnum):
+    NONE = 0
+    DISCORD_EMPLOYEE = 1 << 0
+    PARTNERED_SERVER_OWNER = 1 << 1
+    HYPESQUAD_EVENTS = 1 << 2
+    BUG_HUNTER_LEVEL_1 = 1 << 3
+    HOUSE_BRAVERY = 1 << 6
+    HOUSE_BRILLIANCE = 1 << 7
+    HOUSE_BALANCE = 1 << 8
+    EARLY_SUPPORTER = 1 << 9
+    TEAM_USER = 1 << 10
+    BUG_HUNTER_LEVEL_2 = 1 << 14
+    VERIFIED_BOT = 1 << 16
+    EARLY_VERIFIED_BOT_DEVELOPER = 1 << 17
