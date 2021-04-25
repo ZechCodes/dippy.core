5ea6f94095e2a88a2ec34400785de898c8f0b180
Zech Zimmerman <hi@zech.codes>
2021-04-24 21:24:18 -0400

    Add guild features enum

--- dippy/core/enums.py
+++ dippy/core/enums.py
@@ -187,3 +187,36 @@ class ExplicitContentFilterLevel(IntEnum):
     MEMBERS_WITHOUT_ROLES = 1
     # Media content sent by all members will be scanned
     ALL_MEMBERS = 2
+
+
+class GuildFeature(StrEnum):
+    # Guild has access to set an animated guild icon
+    ANIMATED_ICON = "ANIMATED_ICON"
+    # Guild has access to set a guild banner image
+    BANNER = "BANNER"
+    # Guild has access to use commerce features (i.e. create store channels)
+    COMMERCE = "COMMERCE"
+    # Guild can enable welcome screen, Membership Screening, stage channels & discovery, and receives community updates
+    COMMUNITY = "COMMUNITY"
+    # Guild is able to be discovered in the directory
+    DISCOVERABLE = "DISCOVERABLE"
+    # Guild is able to be featured in the directory
+    FEATURABLE = "FEATURABLE"
+    # Guild has access to set an invite splash background
+    INVITE_SPLASH = "INVITE_SPLASH"
+    # Guild has enabled Membership Screening
+    MEMBER_VERIFICATION_GATE_ENABLED = "MEMBER_VERIFICATION_GATE_ENABLED"
+    # Guild has access to create news channels
+    NEWS = "NEWS"
+    # Guild is partnered
+    PARTNERED = "PARTNERED"
+    # Guild can be previewed before joining via Membership Screening or the directory
+    PREVIEW_ENABLED = "PREVIEW_ENABLED"
+    # Guild has access to set a vanity URL
+    VANITY_URL = "VANITY_URL"
+    # Guild is verified
+    VERIFIED = "VERIFIED"
+    # Guild has access to set 384kbps bitrate in voice (previously VIP voice servers)
+    VIP_REGIONS = "VIP_REGIONS"
+    # Guild has enabled the welcome screen
+    WELCOME_SCREEN_ENABLED = "WELCOME_SCREEN_ENABLED"
