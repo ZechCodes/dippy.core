e80101ba529d27c1b7ab0d6acdd5ae0503216587
Zech Zimmerman <hi@zech.codes>
2021-04-26 16:52:35 -0400

    Add helper methods to the emoji model

--- dippy/core/models/emoji.py
+++ dippy/core/models/emoji.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dippy.core.models.model import DippyCoreCacheableModel
 from dippy.core.models.role import RoleModel
 from dippy.core.models.user import UserModel
@@ -15,3 +16,16 @@ class EmojiModel(DippyCoreCacheableModel):
     require_colons: Optional[bool]
     roles: Optional[list[RoleModel]]
     user: Optional[UserModel]
+
+    def __str__(self):
+        if self.id:
+            return f"{self.name}:{self.id}"
+        return self.name
+
+    @classmethod
+    def parse(cls, emoji: str) -> EmojiModel:
+        if isinstance(emoji, cls):
+            return emoji
+
+        name, _, emoji_id = emoji.partition(":")
+        return cls(name=name, id=int(emoji_id) if emoji_id else None)
