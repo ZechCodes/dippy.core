58fbe0af3d44cd8b40c901ec510def4a56163859
Zech Zimmerman <hi@zech.codes>
2021-09-05 16:31:39 -0400

    Remove old imports

--- dippy/core/__init__.py
+++
@@ -1,8 +0,0 @@
-from dippy.core.caching.manager import CacheManager
-from dippy.core.events import EventDispatch
-from dippy.core.gateway.connection import GatewayConnection
-from dippy.core.gateway.payload import Payload
-from dippy.core.intents import Intents
-
-
-__all__ = ["CacheManager", "EventDispatch", "GatewayConnection", "Intents"]
