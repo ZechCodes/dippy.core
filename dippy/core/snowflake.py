8777718622c48a994ee4a7372dcf943f55096c3c
Zech Zimmerman <hi@zech.codes>
2021-09-05 16:45:20 -0400

    Make snowflakes not ints

--- dippy/core/snowflake.py
+++ dippy/core/snowflake.py
@@ -3,29 +3,39 @@ from dippy.core.datetime_helpers import datetime, from_timestamp
 from functools import cached_property
 
 
-class Snowflake(int):
-    """Simple class for parsing Discord snowflakes. Inherit from int since a snowflake is just an int with special
-    properties. Overrides dunder eq to enforce matching only snowflakes.
+class Snowflake:
+    """Simple class for parsing Discord snowflakes.
 
     Provides instance properties for accessing the snowflake's increment, process ID, worker ID, and timestamp."""
 
     __discord_epoch_offset_ms = 1420070400000
 
+    def __init__(self, snowflake: int):
+        self._snowflake = snowflake
+
     @cached_property
     def increment(self) -> int:
-        return self & self.__mask(12)
+        return self._snowflake & self.__mask(12)
 
     @cached_property
     def process_id(self) -> int:
-        return self >> 12 & self.__mask(5)
+        return self._snowflake >> 12 & self.__mask(5)
 
     @cached_property
     def worker_id(self) -> int:
-        return self >> 17 & self.__mask(5)
+        return self._snowflake >> 17 & self.__mask(5)
 
     @cached_property
     def timestamp(self) -> datetime:
-        return from_timestamp(((self >> 22) + self.__discord_epoch_offset_ms) / 1000)
+        return from_timestamp(
+            ((self._snowflake >> 22) + self.__discord_epoch_offset_ms) / 1000
+        )
+
+    def __eq__(self, other):
+        return isinstance(other, Snowflake) and hash(other) == hash(self)
+
+    def __hash__(self):
+        return self._snowflake
 
     def __mask(self, size: int) -> int:
         return 2 ** size - 1
