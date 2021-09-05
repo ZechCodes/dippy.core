d2df9ebe9bb7959d39b51e00335f17ffbeae5ef2
Zech Zimmerman <hi@zech.codes>
2021-09-05 15:42:47 -0400

    Add some datetime helpers to ensure datetimes are UTC localized

---
+++ dippy/core/datetime_helpers.py
@@ -0,0 +1,23 @@
+"""Simple datetime helper module to ensure that all datetimes are UTC localized."""
+
+
+from __future__ import annotations
+import pendulum
+
+
+datetime = pendulum.datetime  # Just for convenience so we only need one import
+
+
+def now() -> pendulum.datetime:
+    """Gets a datetime for the current time, that is localized to UTC."""
+    return pendulum.now(pendulum.UTC)
+
+
+def from_string(iso8601_datetime_string: str) -> pendulum.datetime:
+    """Converts a datetime string to a datetime that is localized to UTC."""
+    return pendulum.parse(iso8601_datetime_string, tz=pendulum.UTC)
+
+
+def from_timestamp(timestamp: int | float) -> pendulum.datetime:
+    """Converts a timestamp to a datetime that is localized to UTC."""
+    return pendulum.from_timestamp(timestamp, pendulum.UTC)
