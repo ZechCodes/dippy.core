65cb0114b8b8503ab9cff85a8535d720626be362
Zech Zimmerman <hi@zech.codes>
2021-09-05 15:14:12 -0400

    Get rid of old implementation

--- dippy/core/models/reaction.py
+++
@@ -1,8 +0,0 @@
-from dippy.core.models.emoji import EmojiModel
-from dippy.core.models.model import DippyCoreModel
-
-
-class ReactionModel(DippyCoreModel):
-    count: int
-    me: bool
-    emoji: EmojiModel
