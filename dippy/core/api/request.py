8aaa58acbcf4615f81c878cc0410db6638ff2aa8
Zech Zimmerman <hi@zech.codes>
2021-09-06 22:26:06 -0400

    Automatically setup request model field converters

--- dippy/core/api/request.py
+++ dippy/core/api/request.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from attr import attrs, attrib, converters, validators
+from attr import attrs, attrib, Attribute, converters, validators, resolve_types
 from dippy.core.models.base_model import BaseModel
-from typing import Callable, Optional, Protocol, TypeVar
+from typing import Callable, Optional, Protocol, TypeVar, Union, get_args, get_origin
 
 
 NOT_SET = type("NOT_SET", (object,), {"__repr__": lambda s: "NOT_SET"})
@@ -10,10 +10,28 @@ _ValidatorArgType = TypeVar("_ValidatorArgType")
 _T = TypeVar("_T")
 
 
-    return attrs(auto_attribs=True)(func)
+def setup_converters(cls, fields: list[Attribute]) -> list[Attribute]:
+    resolve_types(cls, attribs=fields)
+    return [a.evolve(converter=_build_converter(a)) for a in fields]
+
+
+def _build_converter(attribute):
+    converter = attribute.converter
+    if not attribute.converter:
+        converter = attribute.type
+        if get_origin(converter) is Union:
+            args = get_args(converter)
+            converter = args[0]
+            if None in args:
+                converter = converters.default_if_none(converter)
+
+    return lambda value: value if value is NOT_SET else converter(value)
 
 
 def request_model(func):
+    return attrs(field_transformer=setup_converters)(func)
+
+
 @request_model
 class BaseRequest(Protocol):
     endpoint: str
