403ec51910b4f101810bef351068a3e9f06b3063
Zech Zimmerman <hi@zech.codes>
2021-09-06 22:26:24 -0400

    Use automated converters for audit log endpoint

--- dippy/core/api/endpoints/audit_logs.py
+++ dippy/core/api/endpoints/audit_logs.py
@@ -1,24 +1,18 @@
 from __future__ import annotations
-from dippy.core.api.request import converters, model, query_arg, url_arg
+from dippy.core.api.request import request_model, query_arg, url_arg
 from dippy.core.enums.audit_logs import AuditLogEvents
 from dippy.core.snowflake import Snowflake
 from typing import Optional
 
 
-@model
+@request_model
 class GetAuditLogRequest:
     endpoint = "/guilds/{guild_id}/audit-logs"
     method = "GET"
 
-    guild_id: Snowflake = url_arg(converter=Snowflake)
+    guild_id: Snowflake = url_arg()
 
-    user_id: Optional[Snowflake] = query_arg(
-        converter=converters.default_if_none(Snowflake)
-    )
-    action_type: Optional[AuditLogEvents] = query_arg(
-        converter=converters.default_if_none(AuditLogEvents)
-    )
-    before: Optional[Snowflake] = query_arg(
-        converter=converters.default_if_none(Snowflake)
-    )
-    limit: Optional[int] = query_arg(converter=converters.default_if_none(int))
+    user_id: Optional[Snowflake] = query_arg()
+    action_type: Optional[AuditLogEvents] = query_arg()
+    before: Optional[Snowflake] = query_arg()
+    limit: Optional[int] = query_arg()
