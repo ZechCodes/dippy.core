c6a9d7d28a495107f26f11dbc5f6df85ee61fb10
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-08 13:17:14 +0300

    Implement more channel stuff

--- dippy/core/api/endpoints/channels.py
+++ dippy/core/api/endpoints/channels.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from dippy.core.api.request import request_model, url_arg
+from dippy.core.api.request import request_model, query_arg, url_arg
 from dippy.core.snowflake import Snowflake
 
 
@@ -9,3 +9,36 @@ class GetChannelRequest:
     method = "GET"
 
     channel_id: Snowflake = url_arg()
+
+# TODO: Implement PATCH /channels/{channel.id}
+
+
+@request_model
+class DeleteChannelRequest:
+    """Deletes a channel or closes a private message."""
+    endpoint = "/channels/{channel_id}"
+    method = "DELETE"
+
+    channel_id: Snowflake = url_arg()
+
+
+@request_model
+class GetChannelMessagesRequest:
+    endpoint = "/channels/{channel_id}/messages"
+    method = "GET"
+
+    channel_id: Snowflake = url_arg()
+
+    around: Snowflake = query_arg()
+    before: Snowflake = query_arg()
+    after: Snowflake = query_arg()
+    limit: Snowflake = query_arg()
+
+
+@request_model
+class GetChannelMessageRequest:
+    endpoint = "/channels/{channel_id}/messages/{message_id}"
+    method = "GET"
+
+    channel_id: Snowflake = url_arg()
+    message_id: Snowflake = url_arg()
