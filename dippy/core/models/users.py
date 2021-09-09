6cf523033938e0ba3cf3bf62015be3893384d00c
Zech Zimmerman <hi@zech.codes>
2021-09-09 16:36:57 -0400

    Make user a basemodel

--- dippy/core/models/users.py
+++ dippy/core/models/users.py
@@ -1,8 +1,24 @@
 from dippy.core.models.base_model import BaseModel
+from dippy.core.snowflake import Snowflake
+from typing import Optional
 
 
 class User(BaseModel):
-    ...
+    id: Snowflake
+    username: str
+    discriminator: str
+    avatar: Optional[str]
+    bot: Optional[bool]
+    system: Optional[bool]
+    mfa_enabled: Optional[bool]
+    banner: Optional[str]
+    accent_color: Optional[int]
+    locale: Optional[str]
+    verified: Optional[bool]
+    email: Optional[str]
+    flags: Optional[int]
+    premium_type: Optional[int]
+    public_flags: Optional[int]
 
 
 class Member(BaseModel):
