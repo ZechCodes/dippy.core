f22235c68fa2a3667f02414e5a808429c3a657b1
wake-code <87005812+wake-code@users.noreply.github.com>
2021-09-09 19:28:48 +0300

    Implement Emoji

---
+++ dippy/core/models/user.py
@@ -0,0 +1,22 @@
+from attr import attrs, attrib
+from typing import Optional
+from dippy.core.snowflake import Snowflake
+
+
+@attrs(auto_attribs=True)
+class User:
+    id: Snowflake
+    username: str
+    discriminator: str
+    avatar: Optional[str]
+    bot: Optional[bool]
+    system: Optional[bool]
+    mfa_enabled: Optional[bool]
+    banner: Optional[str]
+    accent_color: Optional[int]
+    locale: Optional[str]
+    verified: Optional[bool]
+    email: Optional[str]
+    flags: Optional[int]
+    premium_type: Optional[int]
+    public_flags: Optional[int]
