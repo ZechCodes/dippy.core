923a3f701babf899284a3db805caab713501e7bf
Zech Zimmerman <hi@zech.codes>
2021-09-09 12:53:47 -0400

    Add placeholder for Message model

--- dippy/core/models/messages.py
+++ dippy/core/models/messages.py
@@ -1,11 +1,10 @@
 from attr import attrs, attrib
-
 from dippy.core.api.request import json_arg
-from dippy.core.enums.messages import EmbedType, AllowedMentionsType
 from dippy.core.datetime_helpers import datetime, from_timestamp
-from typing import Optional
-
+from dippy.core.enums.messages import EmbedType, AllowedMentionsType
+from dippy.core.models.base_model import BaseModel
 from dippy.core.snowflake import Snowflake
+from typing import Optional
 
 
 @attrs(auto_attribs=True)
@@ -52,14 +51,18 @@ class EmbedAuthor:
     url: Optional[str]
     icon_url: Optional[str]
     proxy_icon_url: Optional[str]
-    name: Optional[str] = json_arg(validator=lambda inst, attr, value: len(value) <= 256)
+    name: Optional[str] = json_arg(
+        validator=lambda inst, attr, value: len(value) <= 256
+    )
 
 
 @attrs(auto_attribs=True)
 class EmbedFooter:
     icon_url: Optional[str]
     proxy_icon_url: Optional[str]
-    text: Optional[str] = json_arg(validator=lambda inst, attr, value: len(value) <= 2048)
+    text: Optional[str] = json_arg(
+        validator=lambda inst, attr, value: len(value) <= 2048
+    )
 
 
 @attrs(auto_attribs=True)
@@ -80,9 +83,15 @@ class Embed:
     provider: Optional[EmbedProvider]
     author: Optional[EmbedAuthor]
     timestamp: Optional[datetime] = attrib(converter=from_timestamp)
-    title: Optional[str] = json_arg(validator=lambda inst, attr, value: len(value) <= 256)
-    description: Optional[str] = json_arg(validator=lambda inst, attr, value: len(value) <= 4096)
-    fields: Optional[list[EmbedField]] = json_arg(validator=lambda inst, attr, value: len(list[EmbedField]) <= 25)
+    title: Optional[str] = json_arg(
+        validator=lambda inst, attr, value: len(value) <= 256
+    )
+    description: Optional[str] = json_arg(
+        validator=lambda inst, attr, value: len(value) <= 4096
+    )
+    fields: Optional[list[EmbedField]] = json_arg(
+        validator=lambda inst, attr, value: len(list[EmbedField]) <= 25
+    )
     type: Optional[EmbedType] = EmbedType.RICH
 
 
@@ -112,3 +121,7 @@ class AllowedMentions:
     roles: list[Snowflake]
     users: list[Snowflake]
     replied_user: bool
+
+
+class Message(BaseModel):
+    ...
