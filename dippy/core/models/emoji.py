d870aa6925ff0fa1ea62d2e12fd88a04f80c11a8
Zech Zimmerman <hi@zech.codes>
2021-09-12 17:49:27 -0400

    Clean up emoji model

--- dippy/core/models/emoji.py
+++ dippy/core/models/emoji.py
@@ -1,16 +1,14 @@
 from attr import attrs
-from typing import Optional
-
-from dippy.core.models.permissions import Role
-from dippy.core.models.user import User
+from dippy.core.models.users import User
 from dippy.core.snowflake import Snowflake
+from typing import Optional
 
 
 @attrs(auto_attribs=True)
 class Emoji:
     id: Optional[Snowflake]
     name: Optional[str]
-    roles: Optional[list[Role.id]]
+    roles: Optional[list[Snowflake]]
     user: Optional[User]
     require_colons: Optional[bool]
     managed: Optional[bool]
