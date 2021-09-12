49d66a44ae0b38a44b7ffa80ffb4f995eae17ed3
Zech Zimmerman <hi@zech.codes>
2021-09-12 17:47:45 -0400

    Add datetime support to converter builder

--- dippy/core/converters.py
+++ dippy/core/converters.py
@@ -1,6 +1,7 @@
 from attr import converters
 from dippy.core.not_set import NOT_SET
 from dippy.core.enums.enums import Enum
+from dippy.core.datetime_helpers import datetime, from_timestamp, from_string
 from typing import get_args, get_origin, Union
 
 
@@ -13,6 +14,12 @@ def build_converter(attribute):
 
     if isinstance(converter, Enum):
         converter = converter.safe_get
+    if issubclass(converter, datetime):
+        converter = (
+            lambda dt: from_timestamp(int(dt))
+            if isinstance(dt, int) or dt.isdigit()
+            else from_string(dt)
+        )
     if optional:
         converter = converters.default_if_none(converter)
 
