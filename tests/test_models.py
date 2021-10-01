61d067d51e51d3cf263fa5a311e367648195d169
Zech Zimmerman <hi@zech.codes>
2021-10-01 12:32:18 -0400

    Tests for models

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,10 @@
-from dippy.core.models.base_model import BaseModel
+from dippy.core.model import Model
 from dippy.core.snowflake import Snowflake
+from typing import Optional
 
 
 def test_model():
-    class TestModel(BaseModel):
+    class TestModel(Model):
         id: Snowflake
         name: str
         enabled: bool = False
@@ -11,4 +12,26 @@ def test_model():
     t = TestModel({"id": 1234567890, "name": "Bob"})
 
     assert Snowflake(1234567890) == t.id
+    assert isinstance(t.id, Snowflake)
     assert t.name == "Bob"
+    assert t.enabled is False
+
+    t.enabled = True
+    assert t.enabled
+
+    t.name = 1337
+    assert isinstance(t.name, str)
+    assert t.name == "Bob"
+
+
+def test_model_optional():
+    class TestModel(Model):
+        id: Optional[Snowflake]
+
+    t = TestModel({"id": 1234567890})
+
+    assert isinstance(t.id, Snowflake)
+
+    t = TestModel({})
+
+    assert t.id is None
