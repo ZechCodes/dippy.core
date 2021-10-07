9af4a96365a9da396d1074626227a2f813c0dace
wake-code <87005812+wake-code@users.noreply.github.com>
2021-10-07 16:16:25 +0300

    [ closes #20 ] Presences use declarative models

--- dippy/core/api/models/presences.py
+++ dippy/core/api/models/presences.py
@@ -1,20 +1,18 @@
-from attr import attrs
 from typing import Optional
 from dippy.core.enums.users import Status
 from dippy.core.api.models.activity import Activity
 from dippy.core.api.models.users import User
+from dippy.core.model.models import Model
 from dippy.core.snowflake import Snowflake
 
 
-@attrs(auto_attribs=True)
-class ClientStatus:
+class ClientStatus(Model):
     desktop: Optional[str]
     mobile: Optional[str]
     web: Optional[str]
 
 
-@attrs(auto_attribs=True)
-class Presence:
+class Presence(Model):
     user: User
     guild_id: Snowflake
     status: Status
