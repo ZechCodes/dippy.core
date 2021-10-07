4f36285286a5a5430a989931122cc295ae694778
wake-code <87005812+wake-code@users.noreply.github.com>
2021-10-07 16:20:16 +0300

    [ closes #18 ] Messages use declarative models

--- dippy/core/api/models/messages.py
+++ dippy/core/api/models/messages.py
@@ -1,100 +1,88 @@
-from attr import attrs, attrib
-from dippy.core.api.request import json_arg
+from dippy.core.api.models.emoji import Emoji
 from dippy.core.datetime_helpers import datetime, from_timestamp
 from dippy.core.enums.messages import EmbedType, AllowedMentionsType
-from dippy.core.api.models.base_model import Model
+from dippy.core.model.fields import Field
+from dippy.core.model.models import Model
 from dippy.core.snowflake import Snowflake
 from typing import Optional
 
 
-@attrs(auto_attribs=True)
-class MessageReference:
+class MessageReference(Model):
     message_id: Optional[Snowflake]
     channel_id: Optional[Snowflake]
     guild_id: Optional[Snowflake]
     fail_if_not_exists: Optional[bool]
 
 
-@attrs(auto_attribs=True)
-class FollowedChannel:
+class FollowedChannel(Model):
     channel_id: Snowflake
     webhook_id: Snowflake
 
 
-@attrs(auto_attribs=True)
-class Reaction:
+class Reaction(Model):
     count: int
     me: bool
-    emoji: Snowflake  # FIXME: Emoji
+    emoji: Emoji
 
 
-@attrs(auto_attribs=True)
-class Overwrite:
+class Overwrite(Model):
     id: Snowflake
     type: int
     allow: str
     deny: str
 
 
-@attrs(auto_attribs=True)
-class ThreadMetadata:
+class ThreadMetadata(Model):
     archived: bool
     auto_archive_duration: int
-    archive_timestamp: datetime = attrib(converter=from_timestamp)
+    archive_timestamp: datetime = Field(converter=from_timestamp)
     locked: bool
     invitable: Optional[bool]
 
 
-@attrs(auto_attribs=True)
-class ThreadMember:
+class ThreadMember(Model):
     id: Optional[Snowflake]
     user_id: Optional[Snowflake]
-    join_timestamp: datetime = attrib(converter=from_timestamp)
+    join_timestamp: datetime = Field(converter=from_timestamp)
     flags: int
 
 
-@attrs(auto_attribs=True)
-class EmbedAsset:
+class EmbedAsset(Model):
     url: Optional[str]
     proxy_url: Optional[str]
     height: Optional[int]
     width: Optional[int]
 
 
-@attrs(auto_attribs=True)
-class EmbedProvider:
+class EmbedProvider(Model):
     name: Optional[str]
     url: Optional[str]
 
 
-@attrs(auto_attribs=True)
-class EmbedAuthor:
+class EmbedAuthor(Model):
     url: Optional[str]
     icon_url: Optional[str]
     proxy_icon_url: Optional[str]
-    name: Optional[str] = json_arg(
+    name: Optional[str] = Field(
         validator=lambda inst, attr, value: len(value) <= 256
     )
 
 
-@attrs(auto_attribs=True)
-class EmbedFooter:
+class EmbedFooter(Model):
     icon_url: Optional[str]
     proxy_icon_url: Optional[str]
-    text: Optional[str] = json_arg(
+    text: Optional[str] = Field(
         validator=lambda inst, attr, value: len(value) <= 2048
     )
 
 
-@attrs(auto_attribs=True)
-class EmbedField:
-    name: str = json_arg(validator=lambda inst, attr, value: len(value) <= 256)
-    value: str = json_arg(validator=lambda inst, attr, value: len(value) <= 1024)
+class EmbedField(Model):
+    name: str = Field(validator=lambda inst, attr, value: len(value) <= 256)
+    value: str = Field(validator=lambda inst, attr, value: len(value) <= 1024)
     inline: Optional[bool] = False
 
 
-@attrs(auto_attribs=True)
-class Embed:
+class Embed(Model):
     url: Optional[str]
     color: Optional[int]
     footer: Optional[EmbedFooter]
@@ -103,21 +91,20 @@ class Embed:
     video: Optional[EmbedAsset]
     provider: Optional[EmbedProvider]
     author: Optional[EmbedAuthor]
-    timestamp: Optional[datetime] = attrib(converter=from_timestamp)
-    title: Optional[str] = json_arg(
+    timestamp: Optional[datetime] = Field(converter=from_timestamp)
+    title: Optional[str] = Field(
         validator=lambda inst, attr, value: len(value) <= 256
     )
-    description: Optional[str] = json_arg(
+    description: Optional[str] = Field(
         validator=lambda inst, attr, value: len(value) <= 4096
     )
-    fields: Optional[list[EmbedField]] = json_arg(
+    fields: Optional[list[EmbedField]] = Field(
         validator=lambda inst, attr, value: len(list[EmbedField]) <= 25
     )
     type: Optional[EmbedType] = EmbedType.RICH
 
 
-@attrs(auto_attribs=True)
-class Attachment:
+class Attachment(Model):
     id: Snowflake
     filename: str
     content_type: Optional[str]
@@ -128,16 +115,14 @@ class Attachment:
     width: Optional[int]
 
 
-@attrs(auto_attribs=True)
-class ChannelMention:
+class ChannelMention(Model):
     id: Snowflake
     guild_id: Snowflake
     type: int
     name: str
 
 
-@attrs(auto_attribs=True)
-class AllowedMentions:
+class AllowedMentions(Model):
     parse: list[AllowedMentionsType]
     roles: list[Snowflake]
     users: list[Snowflake]
