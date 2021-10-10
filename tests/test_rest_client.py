7cbcc7f704c15e0a1e72bc1cda83fcaa99b98c31
Zech Zimmerman <hi@zech.codes>
2021-10-10 17:02:11 -0400

    Add tests for request model rewrite

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -6,8 +6,36 @@ from dippy.core.api.endpoints.users import GetUser
 from dippy.core.cache.manager import CacheManager
 from dippy.core.api.models.guilds import Member
 from dippy.core.snowflake import Snowflake
+from dippy.core.api.request import (
+    RequestModel,
+    JSONArgField,
+    QueryArgField,
+    URLArgField,
+)
 from pytest import mark
 from os import getenv
+
+
+def test_request_models():
+    class TestModel(RequestModel):
+        uarg_a: str = URLArgField()
+        uarg_b: str = URLArgField()
+        uarg_c: str = URLArgField()
+
+        jarg_a: int = JSONArgField()
+        jarg_b: int = JSONArgField()
+        jarg_c: int = JSONArgField()
+
+        qarg_a: str = QueryArgField()
+        qarg_b: str = QueryArgField()
+        qarg_c: str = QueryArgField()
+
+    m = TestModel(
+        "a", "b", "c", jarg_a=1, jarg_b=2, jarg_c=3, qarg_a="z", qarg_b="y", qarg_c="x"
+    )
+    assert m.url_args == {"uarg_a": "a", "uarg_b": "b", "uarg_c": "c"}
+    assert m.json_args == {"jarg_a": 1, "jarg_b": 2, "jarg_c": 3}
+    assert m.query_args == {"qarg_a": "z", "qarg_b": "y", "qarg_c": "x"}
 
 
 @mark.asyncio
