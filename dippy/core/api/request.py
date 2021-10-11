f03c966233e306a311c0e4215ac9bc37055b3f99
Zech Zimmerman <hi@zech.codes>
2021-10-10 21:45:59 -0400

    Use data model for request models

--- dippy/core/api/request.py
+++ dippy/core/api/request.py
@@ -1,5 +1,6 @@
 from __future__ import annotations as _
-from dippy.core.model.fields import Field as _Field, NOTSET as _FIELD_NOTSET
+from dippy.core.model.fields import Field as _Field
+from dippy.core.model.data_models import DataModel as _DataModel
 from dippy.core.model.models import Model as _Model
 from dippy.core.model.metadata import MetadataProtocol as _MetadataProtocol
 from dippy.core.sentinel import Sentinel as _Sentinel
@@ -21,18 +22,17 @@ class URLArgField(_Field):
     ...
 
 
-class RequestModel(_Model):
+class RequestModel(_DataModel):
     endpoint: str
     method: str
     model: _Optional[_Model] = None
 
-    def __init__(self: _MetadataProtocol, *args, **kwargs):
-        state = {
+    def _load_fields(self, *args, **kwargs) -> dict[str, _Any]:
+        return {
             **self._get_field_args(JSONArgField, args, kwargs),
             **self._get_field_args(QueryArgField, args, kwargs),
             **self._get_field_args(URLArgField, args, kwargs, allow_positional=True),
         }
-        super().__init__(state)
 
     @property
     def json_args(self: _MetadataProtocol) -> dict[str, _Any]:
@@ -46,30 +46,3 @@ class RequestModel(_Model):
     def url_args(self: _MetadataProtocol) -> dict[str, _Any]:
         return self.__dippy_metadata__.get_for(self, URLArgField)
 
-    def _get_field_args(
-        self: _MetadataProtocol,
-        descriptor: _Field,
-        args: tuple[_Any],
-        kwargs: dict[str, _Any],
-        *,
-        allow_positional: bool = False,
-        index: int = 0,
-    ) -> dict[str, _Any]:
-        values = {}
-        for name in self.__dippy_metadata__[descriptor]:
-            field: URLArgField = getattr(type(self), name)
-            if name in kwargs:
-                value = kwargs[name]
-            elif allow_positional and index < len(args):
-                value = args[index]
-                index += 1
-            elif field.default is not _FIELD_NOTSET:
-                value = field.default
-            else:
-                raise TypeError(
-                    f"__init__() missing 1 required positional argument: '{name}'"
-                )
-
-            values[name] = value
-
-        return values
