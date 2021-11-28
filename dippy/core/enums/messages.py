cc68ecbd816a60691b4c3cd2a6006732b313f805
Zech Zimmerman <hi@zech.codes>
2021-11-28 17:28:26 -0500

    Add component enums for types and styles

--- dippy/core/enums/messages.py
+++ dippy/core/enums/messages.py
@@ -47,3 +47,17 @@ class AllowedMentionsType(StrEnum):
     ROLE_MENTIONS = "roles"  # Controls role mentions
     USER_MENTIONS = "users"  # Controls user mentions
     EVERYONE_MENTIONS = "everyone"  # Controls @everyone and @here mentions
+
+
+class ComponentType(IntEnum):
+    ACTION_ROW = 1  # A container for other components
+    BUTTON = 2  # A button object
+    SELECT_MENU = 3  # A select menu for picking from choices
+
+
+class ButtonStyle(IntEnum):
+    PRIMARY = 1  # blurple
+    SECONDARY = 2  # grey
+    SUCCESS = 3  # green
+    DANGER = 4  # red
+    LINK = 5  # grey, navigates to a URL
